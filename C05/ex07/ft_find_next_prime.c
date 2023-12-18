0f0ee5a6ea7b3a9b0b23d3c314c8f8747924816e
drisanches <driellysanches.s@gmail.com>
2023-12-18 15:43:18 -0300

    inclusion of exercises from the C05 project

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-fati <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 18:43:11 by dde-fati          #+#    #+#             */
+/*   Updated: 2023/09/07 19:23:42 by dde-fati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	is_prime;
+
+	is_prime = ft_is_prime(nb);
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
